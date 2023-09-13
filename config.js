cfad6a84a0e88d38f05d5e44feeb6bad771ef658
mochiidyt <144895610+mochiidyt@users.noreply.github.com>
2023-09-13 16:11:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['~']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '6285175363668': { // put your number here
